648abf897c0b2a708191d1be088ec9de9af69241
Samba <samba.pendela@gmail.com>
2021-08-19 08:50:12 +0530

    javascript started

---
+++ demo.js
@@ -0,0 +1,61 @@
+console.log("welcome to javascript");
+//welcome to javascript
+console.log( 10+10 );                           //20
+console.log( 10+"10" );                         //1010
+console.log( 10+ +"10");                        //20
+console.log( 10 - "10" );                       //0
+console.log( 10 * "10" );                       //100
+console.log( "10" / 10 );                       //1
+console.log( "10"+10-10 );                      //1000
+//==(data)      ===(data & datatype)
+console.log( 10 == "10" );                      //true
+console.log( 10 === "10");                      //false
+console.log( 10 == "ten");                      //false
+console.log( 10 === "ten" );                    //false
+//boolean
+//true          ==> 1
+//false         ==> 0
+console.log( 1+true );                          //2
+console.log( true+true );                       //2
+console.log( true-false );                      //1
+console.log( true/true );                       //1
+console.log( true/false );                      //Infinity
+console.log( 10>9>8 );                          //false
+console.log( 8<9<10 );                          //true
+console.log( 10+10 === 20 );                    //true
+console.log( 0.1+0.2 == 0.30000000000000004 );  //true                       //false
+//number
+//string
+//boolean
+//undefined
+//null
+//bigint
+//Symbol
+//typeof
+console.log( typeof "Hello" );                  //string
+console.log( typeof 100 );                      //number
+console.log( typeof true );                     //boolean
+console.log( typeof undefined );                //undefined
+console.log( typeof null );                     //object
+console.log( typeof [] );                       //object
+console.log( typeof function fun_one(){} );     //function
+console.log( typeof 10n );                      //bigint
+console.log( typeof Symbol(100) );              //symbol
+
+
+//variables
+//variables are used to "store" the data
+//Ex. string, number , boolean ,  arrays ,  objects,..........
+//var ,  let or const
+//let & const keywords introduced in "ES6"
+//a-z , A-Z , 0-9 , $ and _
+//should not start with numbers
+//Syntax :
+// var/let/const variablename = value; 
+
+
+
+
+
+
+
